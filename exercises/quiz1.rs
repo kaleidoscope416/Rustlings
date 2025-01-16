904ed364b613c56fa23c8a1700737bdf225b69d9
kaleido <2448956191@qq.com>
2025-01-16 11:56:42 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(x:u32)->u32 {
+    if x<=40 {
+        2*x
+    }else{
+        x
+    }
+}
 
 // Don't modify this function!
 #[test]
